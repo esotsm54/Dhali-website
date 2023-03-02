98b55433982ce2a608358872a04c922d1c0bbf49
David Simmons <dr.desimmons@gmail.com>
2023-03-02 11:11:15 +0000

    Remove shrinkWrap

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: ListView(
-          shrinkWrap: true,
           children: <Widget>[
             Align(
               alignment: Alignment.bottomCenter,
